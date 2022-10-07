86f2a242fdbe0e27973405469c9eabc16ab3668b
SergeyMC9730 <bdsservers@yandex.ru>
2022-10-07 15:50:15 +0800

    ProcessAudioCheckBox error workaround

--- app/NtscApp.py
+++ app/NtscApp.py
@@ -241,7 +241,7 @@ class NtscApp(QtWidgets.QMainWindow, mainWindow.Ui_MainWindow):
             pass
 
     def audio_filtering(self):
-        state = self.ProcessAudioCheckBox.isChecked()
+        state = False # Workaround
         self.ProcessAudio = state
         try:
             self.videoRenderer.process_audio = state
